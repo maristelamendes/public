c322d128d8c5a5b7dc54778c5a6794f3c3dbb745
Maristela Mendes <istelamms@gmail.com>
2020-11-22 16:35:20 -0300

    first commit

---
+++ entities/Gender.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Gender = void 0;
+var Gender;
+(function (Gender) {
+    Gender["Female"] = "f";
+    Gender["Male"] = "m";
+})(Gender = exports.Gender || (exports.Gender = {}));
+exports.default = Gender;
---
+++ tests.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Person_js_1 = __importDefault(require("./entities/Person.js"));
+const pessoa1 = new Person_js_1.default('Maristela', ('New Date')('2020-1-15', T00, 00, 00), Gender.Male);
